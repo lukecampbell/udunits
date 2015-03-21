e3930c1e1367aa9bacd780f2b374871adf5e065e
Luke Campbell <luke.s.campbell@gmail.com>
2015-03-21 16:41:20 -0400

    Added basic conversions, very buggy

--- udunits/unit.py
+++ udunits/unit.py
@@ -6,15 +6,46 @@ import pkg_resources
 import xml.etree.ElementTree as ET
 
 class System:
-    def __init__(self):
-        self.units = {}
-class Unit:
-    def __init__(self, name, symbol, aliases=None, base=False, definition=None):
+    def __init__(self, name=None):
+        self.name = name
+        self.symbol_table = {}
+
+    def add(self, unit):
+        self.symbol_table[unit.symbol.symbol] = unit
+
+    def __getitem__(self, item):
+        if item in self.symbol_table:
+            return self.symbol_table[item]
+        raise KeyError(item)
+
+class Measure:
+    def __init__(self, unit, value):
+        self.unit = unit
+        self.value = value
+
+    def __repr__(self):
+        return '%s %s' % (self.value, self.unit.symbol.symbol)
+
+    def __mul__(self, other):
+        if isinstance(other, Measure):
+            v = other.value
+        v = other
+        return Measure(self.unit, self.value * v)
+
+    def __rmul__(self, other):
+        return Measure(self.unit, self.value * other)
+
+    def __div__(self, other):
+        if isinstance(other, Measure):
+            v = other.value
+        v = other
+        return Measure(self.unit, self.value / v)
+
+class BaseUnit:
+    def __init__(self, name, symbol, aliases=None):
         self.name = name
         self.symbol = symbol
         self.aliases = aliases or []
-        self.base = base
-        self.definition = definition
 
     def is_base(self):
         return self.base
@@ -26,22 +57,67 @@ class Unit:
         aliases = UnitAliases.fromtag(tag)
         base = tag.find('base')
         if base is None:
-            base = False
+            return None # Not a base unit
         base = True
-        definition = tag.find('def')
-        if definition is not None:
-            definition = definition.text.encode('utf-8')
-            parser = Parser()
-            try:
-                tokens = parser.parse(definition)
-            except:
-                print "BAD DEFINITION:", definition
-        return cls(name=name, symbol=symbol, aliases=aliases, base=base, definition=definition)
+        return cls(name=name, symbol=symbol, aliases=aliases)
 
     def __repr__(self):
-        if self.definition is not None:
-            return '<Unit %s %s >' % (self.definition, self.symbol)
-        return '<Unit %s %s >' % (self.symbol, self.name)
+        return "<BaseUnit '%s'>" % self.symbol.symbol
+
+    def measure(self):
+        return Measure(self.base_unit, 1)
+
+class DerivedUnit:
+    def __init__(self, base_unit, constant, symbol=None, aliases=None):
+        if not isinstance(constant, (float, int)):
+            raise ValueError("Invalid constant, must be a valid number")
+        self.constant = constant
+        self.base_unit = base_unit
+        self.symbol = symbol
+        self.aliases = aliases or []
+
+    def measure(self):
+        return Measure(self.base_unit, self.constant)
+
+    def __repr__(self):
+        return "<Unit '%s'>" % self.measure()
+
+    def is_convertible(self, unit):
+        if isinstance(unit, BaseUnit):
+            return unit.symbol.symbol == self.base_unit.symbol.symbol
+        elif isinstance(unit, DerivedUnit):
+            return self.is_convertible(unit.base_unit)
+        raise TypeError("Unkonwn Type")
+
+    def convert(self, unit):
+        if not is_convertible(unit):
+            raise ValueError("Inconvertible Units")
+
+    def __mul__(self, other):
+        if isinstance(other, (BaseUnit, DerivedUnit)):
+            if not self.is_convertible(other):
+                raise ValueError("Inconvertible Units")
+            other = other.measure()
+        measure = self.measure()
+        result = measure * other
+        return DerivedUnit(self.base_unit, result.value)
+
+    def __rmul__(self, other):
+        if not isinstance(other, (int, float)):
+            raise TypeError("unsupported operand type(s) for /: '%s' and 'DerivedUnit'") % type(other)
+        measure = self.measure()
+        result = measure * other
+        return DerivedUnit(self.base_unit, result.value)
+
+    def __div__(self, other):
+        if isinstance(other, (BaseUnit, DerivedUnit)):
+            if not self.is_convertible(other):
+                raise ValueError("Inconvertible Units")
+            other = other.measure()
+        measure = self.measure()
+        result = measure / other
+        return DerivedUnit(self.base_unit, result.value)
+        
 
 class UnitName:
     def __init__(self, singular, plural=None):
@@ -136,26 +212,25 @@ class Prefix:
 
 
 def idea():
+    system = System()
     system_xml = pkg_resources.resource_string(systems.__name__, 'udunits2-base.xml')
     root = ET.fromstring(system_xml)
     for unit_tag in root.findall('unit'):
-        unit = Unit.fromtag(unit_tag)
-        print unit
+        unit = BaseUnit.fromtag(unit_tag)
+        if unit is not None:
+            system.add(unit)
+
 
     prefix_xml = pkg_resources.resource_string(systems.__name__, 'udunits2-prefixes.xml')
     root = ET.fromstring(prefix_xml)
     for prefix_tag in root.findall('prefix'):
         prefix = Prefix.fromtag(prefix_tag)
-        print prefix
-        print '    ' + ', '.join(prefix.symbols)
 
-    common_xml = pkg_resources.resource_string(systems.__name__, 'udunits2-common.xml')
-    root = ET.fromstring(common_xml)
-    for unit_tag in root.findall('unit'):
-        unit = Unit.fromtag(unit_tag)
-        print unit
+    meters = system['m']
+    dam = DerivedUnit(meters, 10)
+    print dam * 20
 
-
+    
 
 if __name__ == '__main__':
     idea()
